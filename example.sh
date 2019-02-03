c752632ff50d46fed5047d2d7b3155886ef7c8ae
tunahansalih <tusame1994>
2019-02-03 15:28:16 +0300

    simulasyon resimleri uzerinde deneme

--- example.sh
+++ example.sh
@@ -7,4 +7,4 @@
 # python main.py --finetune=/tmp3/first350/TensorFlow/Logs/model.ckpt-1000  --log_dir=/tmp3/first350/TensorFlow/Logs/ --image_dir=/tmp3/first350/SegNet-Tutorial/CamVid/train.txt --val_dir=/tmp3/first350/SegNet-Tutorial/CamVid/val.txt --batch_size=5
 
 #for testing
-python main.py --testing="/content/drive/My Drive/logs/segnet_logs/model.ckpt-19999"  --log_dir="/content/drive/My Drive/logs/segnet_logs" --test_dir="nopeTest.txt" --batch_size=1 --save_image=True
+python main.py --testing="/content/drive/My Drive/logs/segnet_logs/model.ckpt-19999"  --log_dir="/content/drive/My Drive/logs/segnet_logs" --test_dir="simulasyon.txt" --batch_size=1 --save_image=True
